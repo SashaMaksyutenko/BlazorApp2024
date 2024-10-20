c00539fec9320fc1bbdd8e07b3388202fb84cdeb
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-10-21 02:25:40 +0300

    Blazor and JavaScript

---
+++ Helper/IJSruntimeExtensions.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.JSInterop;
+namespace BlazorApp2024.Helper
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+           await jsRuntime.InvokeVoidAsync("showToastr","success", message);
+        }
+        public static async Task ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
