a07bd0f65e603e8c5d82c42aee70f6b9f4615839
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-10-22 17:06:46 +0300

    Forms in Blazor

--- Pages/learnBlazor/Models/Product.cs
+++ Pages/learnBlazor/Models/Product.cs
@@ -2,14 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 namespace BlazorApp2024.Pages.learnBlazor.Models
 {
     public class Product
     {
+        public Product()
+        {
+            AvailableAfter = DateOnly.FromDateTime(DateTime.Now);
+        }
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(1, 1000)]
         public double Price { get; set; }
         public bool IsActive { get; set; }
         public IEnumerable<Product_Prop> ProductProperties { get; set; }
+        public Category Category { get; set; }
+        public DateOnly AvailableAfter { get; set; }
+    }
+    public enum Category
+    {
+        Entree,
+        Appetizer,
+        Dessert
     }
 }
